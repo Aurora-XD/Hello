f3831c838714a6da84610e599bfae830f3b5c2c0
yangchen <chen.yang1@thoughtworks.com>
2020-07-19 15:25:01 +0800

    hide action bar

--- app/src/main/java/com/example/hello/LoginActivity.java
+++ app/src/main/java/com/example/hello/LoginActivity.java
@@ -3,12 +3,16 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_login);
     }
 }
